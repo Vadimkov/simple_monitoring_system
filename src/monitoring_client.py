ae2f6c7e78465db92af442245cb1bbbff84c38bd
Vadim Kovalev <vd.kv@ya.ru>
2018-02-12 19:46:15 +0300

    #1: simple formatted report for client

--- src/monitoring_client.py
+++ src/monitoring_client.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 from socket import *
 from protocol import *
 
@@ -39,6 +40,23 @@ def get_expression_report(sock):
     return units
 
 
+def print_report(report):
+    report_by_agents = {}
+    # sort by agents
+    for unit in report:
+        if unit[0] not in report_by_agents:
+            report_by_agents[unit[0]] = []
+
+        report_by_agents[unit[0]].append(unit)
+
+    # print report, sorted by agents
+    for agent in report_by_agents:
+        print("\t%s:" % (agent,))
+        for unit in report_by_agents[agent]:
+            print("\t\t%s: %s" % (unit[1], unit[2]))
+    # done
+
+
 def main():
     args = configure()
     print("Expressin:", args["expression"])
@@ -46,7 +64,8 @@ def main():
 
     sock = send_expression_request(args["expression"], args["type"])
     report = get_expression_report(sock)
-    print(report)
+    print_report(report)
 
 if __name__ == "__main__":
+    log.setLevel(logging.ERROR)
     main()
