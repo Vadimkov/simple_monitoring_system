0f59880298c07c601e48f1137ee080feaddf849f
Vadim Kovalev <vd.kv@ya.ru>
2018-03-04 10:46:25 +0300

    #3: extra tests for monitoring_lib

--- src/test_monitoring_lib.py
+++ src/test_monitoring_lib.py
@@ -32,11 +32,23 @@ class TestBaseMonitoring(unittest.TestCase):
         agent_storage.create_empty_monitoring_db()
         self.monitoring_agent.check_space()
 
+        for obj in self.golden_objects:
+            agent_storage.update_last_requested_object(obj[0], obj[1], obj[2])
+
     def test_check_space(self):
+        """Test write objects to DB:
+        1. Execute check_space for fake objects;
+        2. Verify, that objects saved is correct."""
+
         db_objects = agent_storage.get_full_last_version()
         self.assertTrue(self.golden_objects == db_objects)
 
     def test_check_space_update_objects(self):
+        """Test update already exists objects:
+        1. Execute check_space for fake objects;
+        2. Change objects and execute check_space again;
+        3. Verify that objects updated correct."""
+
         self.monitoring_agent.suffix = " mdf"
         self.monitoring_agent.check_space()
 
@@ -45,6 +57,44 @@ class TestBaseMonitoring(unittest.TestCase):
                           ('space1', 'object3', 'Content of object3 mdf')]
 
         db_objects = agent_storage.get_full_last_version()
+        self.assertTrue(golden_objects == db_objects)
+
+    def test_update_diff(self):
+        """Test how check_space update diff:
+        1. Execute check_space for fake objects;
+        2. Fill 'last_requested' table;
+        3. Change objects and execute check_space again;
+        4. Execute check_space again;
+        5. Verify that all updated objects has been wrote to 'diff' table."""
+
+        self.monitoring_agent.suffix = " mdf"
+        self.monitoring_agent.check_space()
+
+        golden_objects = [('space1', 'object1', 'Content of object1 mdf'),
+                          ('space1', 'object2', 'Content of object2 mdf'),
+                          ('space1', 'object3', 'Content of object3 mdf')]
+
+        db_objects = agent_storage.get_full_diff()
+        self.assertTrue(golden_objects == db_objects)
+
+    def test_remove_object(self):
+        """Test remove one object:
+        1. Execute check_space for fake objects;
+        2. Fill 'last_requested' table;
+        3. Remove object3 and create object4;
+        4. Check that we sent empty content for object3."""
+        objects = ['object1', 'object2', 'object4']
+        self.monitoring_agent.get_objects = MagicMock(return_value=objects)
+
+        self.monitoring_agent.suffix = " mdf"
+        self.monitoring_agent.check_space()
+
+        golden_objects = [('space1', 'object1', 'Content of object1 mdf'),
+                          ('space1', 'object2', 'Content of object2 mdf'),
+                          ('space1', 'object3', ''),
+                          ('space1', 'object4', 'Content of object4 mdf')]
+
+        db_objects = agent_storage.get_full_diff()
         self.assertTrue(golden_objects == db_objects)
 
 
